8748fced6eb01af4e0e903284b0df4a43dee29e1
aleksandr2106 <aleksandrkornienko19992106@gmail.com>
2019-09-18 20:45:11 +0300

    Add hello world

---
+++ projects/Kornienko/HelloWorld/src/App.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import './App.css';
+
+class App extends PureComponent {
+  render() {
+
+    return  (
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+export default App;
